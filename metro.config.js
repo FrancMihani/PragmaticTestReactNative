696f693943a5f82ddc8af778e4391cca8760d621
Franc Mihani <francimihani@gmail.com>
2024-05-17 22:36:42 +0200

    chore: import path shortener

--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,35 @@
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config')
+const path = require('path')
 
-/**
- * Metro configuration
- * https://reactnative.dev/docs/metro
- *
- * @type {import('metro-config').MetroConfig}
- */
-const config = {};
+const config = {
+  resolver: {
+    extraNodeModules: {
+      assets: path.resolve(__dirname, './src/assets'),
+      components: path.resolve(__dirname, './src/components'),
+      constants: path.resolve(__dirname, './src/constants'),
+      data: path.resolve(__dirname, './src/data'),
+      hooks: path.resolve(__dirname, './src/hooks'),
+      i18n: path.resolve(__dirname, './src/i18n'),
+      navigation: path.resolve(__dirname, './src/navigation'),
+      providers: path.resolve(__dirname, './src/providers'),
+      schemas: path.resolve(__dirname, './src/schemas'),
+      screens: path.resolve(__dirname, './src/screens'),
+      services: path.resolve(__dirname, './src/services'),
+      store: path.resolve(__dirname, './src/store'),
+      styles: path.resolve(__dirname, './src/styles'),
+      theme: path.resolve(__dirname, './src/theme'),
+      types: path.resolve(__dirname, './src/types'),
+      utils: path.resolve(__dirname, './src/utils'),
+    },
+  },
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: false,
+      },
+    }),
+  },
+}
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+module.exports = mergeConfig(getDefaultConfig(__dirname), config)
