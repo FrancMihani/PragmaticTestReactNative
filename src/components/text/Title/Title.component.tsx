af735a2c49565b3cad7c6b497a5ae29a171df5f9
Franc Mihani <francimihani@gmail.com>
2024-05-18 06:34:21 +0200

    wip: Title component

---
+++ src/components/text/Title/Title.component.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import { Text } from 'react-native'
+import styles from 'components/text/Title/Title.styles'
+
+import { TextProps as Props } from 'react-native/Libraries/Text/Text'
+export const TitleComponent = (props: Props) => <Text {...props} style={[styles.title]} />
--- src/screens/CryptoTrackerPro/CryptoTrackerProList.Screen.tsx
+++ src/screens/CryptoTrackerPro/CryptoTrackerProList.Screen.tsx
@@ -1,5 +1,11 @@
 import React from 'react'
+import { SafeAreaView } from 'react-native'
+import { Title } from 'components/text/Title'
 
-const CryptoTrackerProListScreen = () => <></>
+const CryptoTrackerProListScreen = () => (
+  <SafeAreaView>
+    <Title>Test</Title>
+  </SafeAreaView>
+)
 
 export default CryptoTrackerProListScreen
